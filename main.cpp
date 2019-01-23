24f999d28e7ad507f72d5d23ba5a4444536b904f
Marc Jimenez <marcjimenez999@gmail.com>
2019-01-23 14:57:11 -0800

    Added some test variables in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
-int main(int argv, char** argc) {
+#include "base.h"
+#include "Pow.h"
+#include "Mult.h"
+#include "Div.h"
+#include "Add.h"
+#include "Sub.h"
+#include "Rand.h"
+#include "Op.h"
+
+int main() {
+	
+	Op* Op1 = new Op(2);
+	Op* Op2 = new Op(3);
+	Pow* Pow1 = new Pow(Op1, Op2);
+	
+	cout << Op1->evaluate() << endl;
+	cout << Op1->stringify() << endl;
+	cout << Pow1->evaluate() << endl;
+	cout << Pow1->stringify() << endl;
 
 	return 0;
-
 }
