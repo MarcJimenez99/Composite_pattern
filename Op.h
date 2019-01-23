e574b40e9a36b3309fd4dabc96dfbea59c09ba5b
Marc Jimenez <marcjimenez999@gmail.com>
2019-01-23 14:04:06 -0800

    Created initial Op.h file

---
+++ Op.h
@@ -0,0 +1,21 @@
+#ifndef OP_H
+#define OP_H
+
+#include "base.h"
+
+class Op : public Base {
+
+	private: 
+		double value = 0;
+	public:
+		Op(int num) {
+			value = num;
+		}
+		double evaluate() {
+			return value; 
+		}
+		string stringify() {
+			to_string(value);
+		}
+
+};
