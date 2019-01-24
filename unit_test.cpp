ef9a8bf4c760b91527ad05b8fb51b71cbd1942b1
Marc Jimenez <marcjimenez999@gmail.com>
2019-01-24 15:56:02 -0800

    Created RandMock class and made more test cases

--- unit_test.cpp
+++ unit_test.cpp
@@ -7,6 +7,7 @@
 #include "Sub.cpp"
 #include "Rand.cpp"
 #include "Op.cpp" 
+#include "RandMock.cpp"
 #include <iostream>
 #include <string>
 
@@ -15,6 +16,11 @@ using namespace std;
 TEST(CompositeTest, OpEvaluateInt) {
 	Op* Op1 = new Op(1);
 	EXPECT_EQ(1.0, Op1->evaluate());
+}
+
+TEST(CompositeTest, RandEvaluate) {
+	RandMock* x = new RandMock();
+	EXPECT_EQ(2.0, x->evaluate());
 }
 
 TEST(CompositeTest, OpEvaluateDouble) {
@@ -32,9 +38,33 @@ TEST(CompositeTest, OpStringifyInt) {
 	EXPECT_EQ("1.000000", Op1->stringify());
 } 
 
+TEST(CompositeTest, RandStringify) {
+	RandMock* x = new RandMock(); 
+	EXPECT_EQ("2.000000", x->stringify());
+}
+
 TEST(CompositeTest, OpStringifyDouble) {
 	Op* Op1 = new Op(1.0);
 	EXPECT_EQ("1.000000", Op1->stringify());
+}
+
+TEST(CompositeTest, OpStringifyNegative) {
+	Op* Op1 = new Op(-1.0);
+	EXPECT_EQ("-1.000000", Op1->stringify());
+}
+
+TEST(CompositeTest, PowDoubleOpEvaluate) {
+	Op* x = new Op(2.0);
+	Op* y = new Op(2.0); 
+	Pow* Pow1 = new Pow(x, y); 
+	EXPECT_EQ(4.0, Pow1->evaluate()); 
+}
+
+TEST(CompositeTest, PowDoubleRandEvaluate) {
+	RandMock* x = new RandMock();
+	Op* y = new Op(2.0);
+	Pow* Pow1 = new Pow(x, y); 
+	EXPECT_EQ(4.0, Pow1->evaluate());
 }
 int main(int argc, char**argv) {
 	::testing::InitGoogleTest(&argc, argv);
